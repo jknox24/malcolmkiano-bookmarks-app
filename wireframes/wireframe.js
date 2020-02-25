ffba779cf0ae730c8ebcfad4f92a604d2420e5ee
Malcolm Kiano <malcolmkiano@gmail.com>
2020-02-25 11:05:49 -0500

    Added easier navigation between wireframes for demo

--- wireframes/wireframe.js
+++ wireframes/wireframe.js
@@ -1,7 +1,13 @@
+/* global $ */
+
 function init() {
   $('#showCode').on('click', function(){
     $('#codeSection').toggleClass('active');
-  })
+  });
+
+  $('#showLinks').on('click', function(){
+    $('#wf-links').toggleClass('active');
+  });
 }
 
 $(init);
