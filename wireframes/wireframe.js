6eece6dc7e1b7f9ac8b0928ca8866b6b64e3c0f0
Malcolm Kiano <malcolmkiano@gmail.com>
2020-02-25 11:20:09 -0500

    Updated store variable for code display

--- wireframes/wireframe.js
+++ wireframes/wireframe.js
@@ -2,10 +2,12 @@
 
 function init() {
   $('#showCode').on('click', function(){
+    $('#wf-links').removeClass('active');
     $('#codeSection').toggleClass('active');
   });
 
   $('#showLinks').on('click', function(){
+    $('#codeSection').removeClass('active');
     $('#wf-links').toggleClass('active');
   });
 }
